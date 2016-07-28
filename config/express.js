0a3b85d70339614cd705eee814923829b9ee5a06
Rafael <rafacgoncalves@gmail.com>
2016-07-28 13:50:29 -0300

    commit após apagar arquivos poe engano

--- config/express.js
+++ config/express.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 module.exports = function(){
 	var app = express();	
-	app.set ('port', 3000);
+	app.set ('port', process.env.PORT || 3000);
 	app.use(express.static('./public')); 
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
