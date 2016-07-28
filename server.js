53dfac6daa626b4a9e0aeeeeea391fbf2ce127c3
Rafael <rafacgoncalves@gmail.com>
2016-07-28 15:17:39 -0300

    Implantado sentry

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+var raven = require('raven');
+var client = new raven.Client('https://1c0d565f50464d88b179b3303e806c4d:ce869b1427b44554a832262302a86fa8@app.getsentry.com/89553');
+client.patchGlobal();
 var http = require('http');
-var app = require('./config/express')();
+var app = require('./config/expresso')();
 //require('./config/database.js')('mongodb://localhost/disseminar');
 require('./config/database.js')('mongodb://master:753951@ds051738.mlab.com:51738/disseminar');
 http.createServer(app).listen(app.get('port'), function () {
