16fc5174eee42b8385e8ced9da3a5bc7c73b2790
Rafael <rafacgoncalves@gmail.com>
2016-10-07 08:09:01 -0300

    Acerto no endereço do servidor de mongo

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
 //client.patchGlobal();
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/disseminar');
-//require('./config/database.js')('mongodb://master:753951@ds051738.mlab.com:51738/disseminar');
+//require('./config/database.js')('mongodb://localhost/disseminar');
+require('./config/database.js')('mongodb://master:753951@ds051738.mlab.com:51738/disseminar');
 http.createServer(app).listen(app.get('port'), function () {
 	console.log('Express Server escutando na porta ' + app.get('port'));
 });
+
 
 
